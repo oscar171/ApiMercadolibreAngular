73fd7d53d27e822db7e37dcb4d7600b855351749
Oscar Perez <oscarp171@gmail.com>
2016-07-29 16:41:49 -0400

    actualizando preguntas y ventas

--- app/controllers/prueba.php
+++ app/controllers/prueba.php
@@ -16,90 +16,159 @@ $bd=Db::getInstance();
 
 if($result)
 {
-	 while ($x=$bd->obtener_fila($result,0)) 
-	 {
-               try{
-	 	switch ($x['topic']) 
-	 	{
-	 		case 'questions':
+  $array='';
+   while ($x=$bd->obtener_fila($result,0)) 
+   {
+       try{
+        switch ($x['topic']) 
+        {
+                case 'questions':
+                $ver= explode('/',$x['recurso']); 
+                $params = array('access_token' => $_SESSION['access_token']);
+                        $result3 = $meli->get('/questions/'.$ver[2], $params);
+                        if($result3['httpCode']==200 || $result3['httpCode']==404 )
+                        {
+                                 $sql="DELETE FROM notificaciones WHERE recurso = '".$x['recurso']."'";
+                                  $bd->ejecutar($sql);
+                                if($result3['body']->status=="UNANSWERED")
+                                {
+                                        $sql="INSERT INTO preguntas (id, seller_id, item_id,status,fechaCreada,pregunta)
+                                VALUES ('".$result3['body']->id."', '".$result3['body']->seller_id."', '".$result3['body']->item_id."','".$result3['body']->status."','".$result3['body']->date_created."','".$result3['body']->text."')";
+                                $respon=$bd->ejecutar($sql);
+                                 $array['mensaje']="primer ciclo: ".$respon;
+                                }
+                                if($result3['body']->status=="ANSWERED")
+                                {
+                                $sql="UPDATE preguntas
+                                SET status='ANSWERED',fechaRespuesta='".$result3['body']->answer->date_created."', respuesta='".$result3['body']->answer->text."' WHERE id='".$result3['body']->id."'";
+                                $respon=$bd->ejecutar($sql);
+                                 $array['mensaje']="segundo ciclo: ".$respon;
+                                }
+                                if($result3['body']->status==404)
+                                {
+                                  $sql="UPDATE preguntas
+                                SET status='eliminada' WHERE id='".$ver[2]."'";
+                                $respon=$bd->ejecutar($sql);
+                                 $array['mensaje']="Pregunta eliminada: ".$respon;
+                                }
+                        }else
+                        {
+                          $array['mensaje']="error al conectar mercadolibre";
+                        }
+
+                        break;
+                case 'created_orders':
+                       $ver= explode('/',$x['recurso']); 
+                                $params = array('access_token' => $_SESSION['access_token']);
+                        $result3 = $meli->get('/orders/'.$ver[2], $params);
+                        if($result3['httpCode']==200)
+                                {
+                                   $sql="SELECT id FROM ventas WHERE id='".$ver[2]."'";
+                                   $bol=$bd->ejecutar($sql);
+                                  if(!$bol)
+                                  {
+                                        $sql="DELETE FROM notificaciones WHERE recurso = '".$x['recurso']."'";
+                                  $bd->ejecutar($sql);
+                                    $sql="INSERT INTO ventas (id_orden, id_buyer, name_buyer,lastname_buyer,nickname_buyer,phone_buyer,fecha_creacion,fecha_expiracion,envio,status,item_id,item_title)
+                                  VALUES ('".$result3['body']->id."', '".$result3['body']->buyer->id."', '".$result3['body']->buyer->first_name."','".$result3['body']->buyer->last_name."','".$result3['body']->buyer->nickname."','".$result3['body']->buyer->phone->number."','".$result3['body']->date_created."','".$result3['body']->expiration_date."','".$result3['body']->shipping->status."','".$result3['body']->status."','".$result3['body']->order_items[0]->item->id."','".$result3['body']->order_items[0]->item->title."')";
+                                  $respon=$bd->ejecutar($sql);
+                                  $array['mensaje']="new_order";
+                                  $array['data']=array("telefono"=>$result3['body']->buyer->phone->number,
+                                      "new_order_id"=>$ver[2]
+                                    );
+                                  
+
+                                  }
+                                  else
+                                  {
+                                      if($result3['body']->feedback->purchase!=NULL)
+                                      {
+                                       
+                                       $sql="UPDATE ventas
+                                      SET rating='".$result3['body']->feedback->purchase->rating."' WHERE id='".$ver[2]."'";
+                                      $respon=$bd->ejecutar($sql);
+                                      if($respon){
+                                      $array['mensaje']="calificacion comprador orden id: ".$ver[2].$respon;
+                                        }
+                                        else{
+                                          print_r($respon);
+                                        }
+                                      }
+                                      else
+                                      {
+                                            $array['mensaje']="no han calificado";
+
+                                      }
+
+
+                                  }
+                              }
+                              else{
+                                $array['mensaje']="error al conectar mercadolibre";
+                              }
+
+                      break;
+                    case 'orders':
                         $ver= explode('/',$x['recurso']); 
-                	$params = array('access_token' => $_SESSION['access_token']);
-                		$result3 = $meli->get('/questions/'.$ver[2], $params);
-                                if($result3['httpCode']==200 || $result3['httpCode']==404 ){
-                                	 $sql="DELETE FROM notificaciones WHERE recurso = '".$x['recurso']."'";
-                 		          $bd->ejecutar($sql);
-					if($result3['body']->status=="UNANSWERED")
-					{
-						$sql="INSERT INTO preguntas (id, seller_id, item_id,status,fechaCreada,pregunta)
-					VALUES ('".$result3['body']->id."', '".$result3['body']->seller_id."', '".$result3['body']->item_id."','".$result3['body']->status."','".$result3['body']->date_created."','".$result3['body']->text."')";
-					$respon=$bd->ejecutar($sql);
-	                                 $array['mensaje']="primer ciclo: ".$respon;
-					}
-					if($result3['body']->status=="ANSWERED")
-					{
-						$sql="UPDATE preguntas
-					SET status='ANSWERED',fechaRespuesta='".$result3['body']->answer->date_created."', respuesta='".$result3['body']->answer->text."' WHERE id='".$result3['body']->id."'";
-					$respon=$bd->ejecutar($sql);
-                                         $array['mensaje']="segundo ciclo: ".$respon;
-					}
-                                        if($result3['body']->status==404)
+                                $params = array('access_token' => $_SESSION['access_token']);
+                        $result3 = $meli->get('/orders/'.$ver[2], $params);
+                        if($result3['httpCode']==200)
+                                {
+                                  
+                                   $sql="SELECT id FROM ventas WHERE id_orden='".$ver[2]."'";
+                                   $bol=$bd->ejecutar($sql);
+                                  if(!$bol){
+                                        /*$sql="DELETE FROM notificaciones WHERE recurso = '".$x['recurso']."'";
+                                  $bd->ejecutar($sql);*/
+                                  $sql="INSERT INTO ventas (id_orden, id_buyer, name_buyer,lastname_buyer,nickname_buyer,phone_buyer,fecha_creacion,fecha_expiracion,envio,status,item_id,item_title)
+                                VALUES ('".$result3['body']->id."', '".$result3['body']->buyer->id."', '".$result3['body']->buyer->first_name."','".$result3['body']->buyer->last_name."','".$result3['body']->buyer->nickname."','".$result3['body']->buyer->phone->number."','".$result3['body']->date_created."','".$result3['body']->expiration_date."','".$result3['body']->shipping->status."','".$result3['body']->status."','".$result3['body']->order_items[0]->item->id."','".$result3['body']->order_items[0]->item->title."')";
+                                $respon=$bd->ejecutar($sql);
+                                 $array['mensaje']="new_order";
+                                 $array['data']=array("telefono"=>$result3['body']->buyer->phone->number,
+                                    "new_order_id"=>$ver[2]
+                                  );
+
+                                    }
+                                    else
+                                    {
+                                      if($result3['body']->feedback->purchase!=NULL){
+                                       $sql="UPDATE ventas
+                                      SET rating='".$result3['body']->feedback->purchase->rating."' WHERE id='".$result3['body']->id."'";
+                                      $respon=$bd->ejecutar($sql);
+                                      if($respon){
+                                      $array['mensaje']="calificacion comprador orden id: ".$ver[2].$respon;
+                                        }else
                                         {
-                                          $sql="UPDATE preguntas
-                                        SET status='eliminada' WHERE id='".$ver[2]."'";
-                                        $respon=$bd->ejecutar($sql);
-                                         $array['mensaje']="tercer ciclo: ".$respon;
+                                          print_r($respon."no hay nada");
                                         }
-                            	}
+                                      }
+                                      else
+                                      {
+                                        $array['mensaje']="no han calificado";
+                                      }
 
-	 			break;
-	 		case 'created_orders':
-	 		       $ver= explode('/',$x['recurso']); 
-                        		$params = array('access_token' => $_SESSION['access_token']);
-                                $result3 = $meli->get('/orders/'.$ver[2], $params);
-                                if($result3['httpCode']==200)
-                                	{
-                                        	$sql="DELETE FROM notificaciones WHERE recurso = '".$x['recurso']."'";
-                                          $bd->ejecutar($sql);
-                                	  $sql="INSERT INTO ventas (id_orden, id_buyer, name_buyer,lastname_buyer,nickname_buyer,phone_buyer,fecha_creacion,fecha_expiracion,envio,status,item_id,item_title)
-VALUES ('".$result3['body']->id."', '".$result3['body']->buyer->id."', '".$result3['body']->buyer->first_name."','".$result3['body']->buyer->last_name."','".$result3['body']->buyer->nickname."','".$result3['body']->buyer->phone->number."','".$result3['body']->date_created."','".$result3['body']->expiration_date."','".$result3['body']->shipping->status."','".$result3['body']->status."','".$result3['body']->order_items[0]->item->id."','".$result3['body']->order_items[0]->item->title."')";
-									 $respon=$bd->ejecutar($sql);
-                                         $array['mensaje']="primer ciclo compra: ".$respon;
+                                    }
+                              }
+                              else
+                              {
+                                $array['mensaje']="error al conectar mercadolibre";
+                              }
+                        break;
+                default:
+                        # code...
+                        break;
+          }              
+        }catch (Exception $e) {
+                echo 'Excepción capturada: ',  $e->getMessage(), "";
+                        }
 
-                                	}
-
-
-
-
-
-	 			break;
-	 		case 'orders':
-	 			$ver= explode('/',$x['recurso']); 
-                                        $params = array('access_token' => $_SESSION['access_token']);
-                                $result3 = $meli->get('/orders/'.$ver[2], $params);
-                                if($result3['httpCode']==200)
-                                        {
-                                          $sql="INSERT INTO ventas (id_orden, id_buyer, name_buyer,lastname_buyer,nickname_buyer,phone_buyer,fecha_creacion,fecha_expiracion,envio,status,item_id,item_title)
-                                        VALUES ('".$result3['body']->id."', '".$result3['body']->buyer->id."', '".$result3['body']->buyer->first_name."','".$result3['body']->buyer->lastname."','".$result3['body']->buyer->nickname."','".$result3['body']->buyer->phone->number."','".$result3['body']->date_create."','".$result3['body']->expiration_date."','".$result3['body']->shipping->status."','".$result3['body']->status."','".$result3['body']->order_items[0]->item->id."','".$result3['body']->order_items[0]->item->title."')";
-                                        $respon=$bd->ejecutar($sql);
-                                         $array['mensaje']="primer ciclo compra: ".$respon;
-
-                                        }
-	 			break;
-	 		default:
-	 			# code...
-	 			break;
-                  }              
-	 	}catch (Exception $e) {
-    			echo 'Excepción capturada: ',  $e->getMessage(), "";
-				}
-
-	      
-              
-	 }
+      
+      
+    }
  }else
  {
-	
-	$array['mensaje']='noData';
+        
+        $array['mensaje']='noData';
 
  }
 //$questions=$result3['body']->questions;
